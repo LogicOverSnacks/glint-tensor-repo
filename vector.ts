ecae7c227b6218883576238c6b7aee47c1430c3f
Bob <bob@example.com>
2022-04-20 13:44:18 +0100

    use ramda to simplify code

--- vector.ts
+++ vector.ts
@@ -1,14 +1,14 @@
+import * as Ramda from 'ramda';
+
 import { Tensor } from './tensor';
 
 export class Vector<Rows extends number> extends Tensor<[Rows]> {
   values: number[] = super.values;
 
   add(rhs: Tensor<[Rows]>): Vector<Rows> {
-    const newValues: number[] = [];
-    for (let i = 0; i < rhs.values.length; i++) {
-      newValues[i] = this.values[i] + rhs.values[i];
-    }
-
-    return new Vector<Rows>(newValues);
+    return new Vector<Rows>(Ramda
+      .zip(this.values, rhs.values)
+      .map(([l, r]) => l + r)
+    );
   }
 }
