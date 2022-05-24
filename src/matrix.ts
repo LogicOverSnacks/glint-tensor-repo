b775ba3b21e7258d1bee7ae21e8ac3b17ac4ecf6
Bob <bob@example.com>
2022-05-24 12:33:17 +0100

    add matrix class

---
+++ src/matrix.ts
@@ -0,0 +1,16 @@
+import * as Ramda from 'ramda';
+
+import { Tensor } from './tensor';
+
+export class Matrix<Columns extends number, Rows extends number> extends Tensor<[Columns, Rows]> {
+  add(rhs: number | Tensor<[Columns, Rows]>): Matrix<Columns, Rows> {
+    if (typeof rhs === 'number') {
+      return super.add(rhs);
+    }
+
+    return new Matrix<Columns, Rows>(Ramda
+      .zip(this.values, rhs.values)
+      .map(([lCols, rCols]) => Ramda.zip(lCols, rCols).map(([l, r]) => l + r))
+    );
+  }
+}
