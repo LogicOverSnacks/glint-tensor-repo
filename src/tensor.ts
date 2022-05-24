631f91a71dd5484efb6bccc96c4d6da9a94ef9ab
Alice <alice@example.com>
2022-05-24 12:03:26 +0100

    allow adding constant values to a Tensor

--- src/tensor.ts
+++ src/tensor.ts
@@ -1,15 +1,45 @@
-export class Tensor<Dimensions extends readonly number[]> {
-  values: any[];
+type ReduceDimension<T extends readonly any[]> = T extends readonly [infer X, ...infer XS] ? XS : number[];
 
-  constructor(values: any[]) {
+type TensorValues<Dimensions extends readonly number[]> = Dimensions extends readonly []
+  ? number
+  : TensorValues<ReduceDimension<Dimensions>>[];
+
+export class Tensor<Dimensions extends readonly number[]> {
+  // prevent duck typing
+  private readonly θtensorDimensions?: Dimensions;
+
+  values: TensorValues<Dimensions>;
+
+  constructor(values: TensorValues<Dimensions>) {
     this.values = values;
   }
 
-  protected is1D(tensor: Tensor<any>): tensor is Tensor<[number]> {
+  add(rhs: number): Tensor<Dimensions> {
+    if (Tensor.isConstant(this)) {
+      return new Tensor<readonly []>(this.values + rhs) as unknown as Tensor<Dimensions>;
+    }
+
+    return new Tensor(Tensor.addValues(this.values, rhs));
+  }
+
+  protected static isConstant(tensor: Tensor<readonly number[]>): tensor is Tensor<readonly []> {
+    return typeof tensor.values === 'number';
+  }
+
+  protected static is1D(tensor: Tensor<readonly number[]>): tensor is Tensor<readonly [number]> {
     return typeof tensor.values[0] === 'number';
   }
 
-  protected is2D(tensor: Tensor<any>): tensor is Tensor<[number, number]> {
-    return typeof tensor.values[0].values?.[0] === 'number';
+  protected static is2D(tensor: Tensor<readonly number[]>): tensor is Tensor<readonly [number, number]> {
+    return typeof tensor.values[0]?.[0] === 'number';
   }
+
+  protected static is3D(tensor: Tensor<readonly number[]>): tensor is Tensor<readonly [number, number]> {
+    return typeof tensor.values[0]?.[0]?.[0] === 'number';
+  }
+
+  private static addValues = <D extends readonly number[]>(values: TensorValues<D>, rhs: number): TensorValues<D> =>
+    typeof values === 'number'
+      ? (values + rhs) as number as TensorValues<D>
+      : values.map(value => Tensor.addValues(value, rhs)) as TensorValues<D>;
 }
--- src/vector.ts
+++ src/vector.ts
@@ -3,9 +3,11 @@ import * as Ramda from 'ramda';
 import { Tensor } from './tensor';
 
 export class Vector<Rows extends number> extends Tensor<[Rows]> {
-  values: number[] = super.values;
+  add(rhs: number | Tensor<[Rows]>): Vector<Rows> {
+    if (typeof rhs === 'number') {
+      return super.add(rhs);
+    }
 
-  add(rhs: Tensor<[Rows]>): Vector<Rows> {
     return new Vector<Rows>(Ramda
       .zip(this.values, rhs.values)
       .map(([l, r]) => l + r)
